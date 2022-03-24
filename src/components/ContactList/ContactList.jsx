46fbcf0beab110dee21429de1df16f490190e264
DenisMalv <kissed03@gmail.com>
2022-03-24 13:18:50 +0200

    функция вместо класса в contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,38 +1,18 @@
-import React,{ Component } from "react";
+import React from "react";
 import propTypes from "prop-types";
 import css from './ContactList.module.css'
 import ContactItem from "components/ContactItem/ContactItem";
 
-class ContactList extends Component {
-    static propTypes = {
-        contacts: propTypes.arrayOf(propTypes.exact({
-            id: propTypes.string.isRequired,
-            name: propTypes.string.isRequired,
-            number: propTypes.string.isRequired,
-        })),
-        deleteContact: propTypes.func.isRequired
-    }
-
-    handleDelete = contId => {
-        this.props.deleteContact(contId)
-    }
-
-    render() {
-        const { contacts } = this.props
-        const { handleDelete } = this
-        
-        return (
+const ContactList = ({ contacts,deleteContact }) => {
+    return (
             <ul className={css.contactList}>
                 {
                 contacts.map(({id,name,number}) =>
-                    <ContactItem id={ id }name={ name } number={ number } onDelete={ handleDelete } key={ id }/>
+                    <ContactItem id={ id }name={ name } number={ number } onDelete={ deleteContact } key={ id }/>
                     )
                 }  
             </ul>
         )
-        
-    }
-
 }
 
 ContactList.propTypes = {
